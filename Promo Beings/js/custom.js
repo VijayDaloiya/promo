e66135e2203172f2e3476180cb06f07a9068deec
VijayDaloiya <63780543+VijayDaloiya@users.noreply.github.com>
2021-03-05 12:02:25 -0800

    Add files via upload

---
+++ Promo Beings/js/custom.js
@@ -0,0 +1,68 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // PARALLAX EFFECT
+    $.stellar({
+      horizontalScrolling: false,
+    });
+
+
+    // ABOUT SLIDER
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items: 1,
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+
+
+
+  //  $(document).ready(function(){
+  //      $('.counter-value').each(function(){
+  //          $(this).prop('Counter',0).animate({
+  //              Counter: $(this).text()
+  //          },{
+  //              duration: 3500,
+  //              easing: 'swing',
+  //              step: function (now){
+  //                  $(this).text(Math.ceil(now));
+  //              }
+  //          });
+  //      });
+  //  });
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });
+
+})(jQuery);
